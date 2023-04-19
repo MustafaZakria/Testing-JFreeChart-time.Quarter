12756f0d39b581a647643dea4d3251226f49e4d8
Mustafa Zakria <mustafa.zakria21@gmail.com>
2023-04-19 22:28:40 +0200

    base structure

---
+++ JFreeChart/src/org/jfree/data/test/QuarterClassTest.java
@@ -0,0 +1,24 @@
+package org.jfree.data.test;
+import static org.junit.Assert.assertEquals;
+import org.jfree.data.time.Quarter;
+import org.junit.Test;
+
+public class QuarterClassTest {
+    Quarter quarter;
+
+    private void arrange(Integer quart, Integer year) {
+        quarter = new Quarter(quart, year);
+    }
+
+    private void arrange() {
+        quarter = new Quarter();
+    }
+    @Test
+    public void testQuarterDefaultCtor() {
+        arrange();
+        assertEquals(2023, quarter.getYear().getYear());
+        assertEquals(2, quarter.getQuarter());
+    }
+
+
+}
